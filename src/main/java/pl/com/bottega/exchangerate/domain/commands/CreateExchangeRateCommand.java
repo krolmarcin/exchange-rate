a71631135bbcc2e6d561816f11fbc38ef879f04c
Marcin <szeryfmk@gmail.com>
2017-05-17 08:29:11 +0200

    polishing

--- src/main/java/pl/com/bottega/exchangerate/domain/commands/CreateExchangeRateCommand.java
+++ src/main/java/pl/com/bottega/exchangerate/domain/commands/CreateExchangeRateCommand.java
@@ -35,16 +35,36 @@ public class CreateExchangeRateCommand implements Validatable {
 
     @Override
     public void validate(ValidationErrors errors) {
-        if (isEmpty(date))
-            errors.add("date", "is required");
-        if (isEmpty(currency))
-            errors.add("currency", "is required");
-        if (currency != null && !(currency.trim().length() == 3))
-            errors.add("currency", "has invalid format");
-        if (rate == null || isEmpty(rate))
-            errors.add("rate", "is required");
+        validateEmptyDate(errors);
+        validateEmptyCurrency(errors);
+        validateCurrencyFormat(errors);
+        validateEmptyRate(errors);
+        validateRateSmallerThenZero(errors);
+    }
+
+    private void validateRateSmallerThenZero(ValidationErrors errors) {
         if (rate != null && (rate.compareTo(BigDecimal.ZERO) < 0))
             errors.add("rate", "must be > than 0.0");
+    }
+
+    private void validateEmptyRate(ValidationErrors errors) {
+        if (rate == null || isEmpty(rate))
+            errors.add("rate", "is required");
+    }
+
+    private void validateCurrencyFormat(ValidationErrors errors) {
+        if (currency != null && !(currency.trim().length() == 3))
+            errors.add("currency", "has invalid format");
+    }
+
+    private void validateEmptyCurrency(ValidationErrors errors) {
+        if (isEmpty(currency))
+            errors.add("currency", "is required");
+    }
+
+    private void validateEmptyDate(ValidationErrors errors) {
+        if (isEmpty(date))
+            errors.add("date", "is required");
     }
 
 }
